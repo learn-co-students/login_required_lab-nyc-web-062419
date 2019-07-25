0ffc362585c67862f5053475e28c629467b2ed11
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-25 16:33:24 -0400

    login required

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+get "/login", to: "sessions#new"
+post "/login", to: "sessions#create"
+get "/login/welcome", to: "sessions#welcome"
+get '/secrets', to: "secrets#show"
+post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
